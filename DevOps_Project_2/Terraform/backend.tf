ce5666be6f1b29585e7a7d025f55cc7ba95bb78d
iam-shravanigajul <shravanirameshgajul@gmail.com>
2024-06-08 23:50:53 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile-sgajul"
+    bucket = "euran-terraform-statefile-sgajul08jun2024"
     key = "server_name/statefile"
     region = "ap-south-1"
   }
